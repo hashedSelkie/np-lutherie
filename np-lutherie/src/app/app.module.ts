519ce757048dfb9706b2c3288a0bcdb4ba5e6ede
Martina <mckurin@gmail.com>
2023-03-03 13:44:08 +0000

    Menu without link interaction

---
+++ np-lutherie/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { InstrumentsComponent } from './instruments/instruments.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    InstrumentsComponent,
+    ServicesComponent,
+    ContactComponent,
+    MenuComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: 'home', component: HomeComponent, data: { state: 'home' }},
+      {path: 'about', component: AboutComponent,  data: { state: 'about' }},
+      {path: 'instruments', component: InstrumentsComponent, data: { state: 'instruments' }},
+      {path: 'services', component: ServicesComponent, data: { state: 'services' }},
+      {path: 'contact', component: ContactComponent, data: { state: 'contact' }},
+      {path: '**', redirectTo: '/home', pathMatch: 'full'},
+  
+    ]),
+    HttpClientModule,
+    AngularSvgIconModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
