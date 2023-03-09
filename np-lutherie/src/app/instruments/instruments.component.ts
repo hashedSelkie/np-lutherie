6dbc28cbf5ed485678cb60a565aebefb8ce25bbb
Martina <mckurin@gmail.com>
2023-03-09 18:10:20 +0000

    first iteration of gallery

--- np-lutherie/src/app/instruments/instruments.component.ts
+++ np-lutherie/src/app/instruments/instruments.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-instruments',
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./instruments.component.scss']
 })
 export class InstrumentsComponent {
+ 
+  enlarge(event: any){
+    let src: string = (event.target as HTMLImageElement).src;
+    let shower = document.getElementById("shower");
+    shower.style.display = "block"
+    let view = document.getElementById("viewed-image");
+    let oldsrc = view as HTMLImageElement;
+    oldsrc.src = src;
+  }
 
+  close(event: any){
+    let shower = document.getElementById("shower");
+    shower.style.display = "none"
+   console.log("x has been clicked");
+  }
+ 
 }
