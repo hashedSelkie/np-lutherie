613965a7ddac441448033d151a20bfa26d5729dd
Martina <mckurin@gmail.com>
2023-03-10 14:41:31 +0000

    Testimonials and cursor pointer

--- np-lutherie/src/app/home/home.component.ts
+++ np-lutherie/src/app/home/home.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  enlarge(event: any){
+    let src: string = (event.target as HTMLImageElement).src;
+    let type: string = (event.target as HTMLImageElement).className;
+    let shower = document.getElementById("shower");
+    shower.style.display = "block"
+    let view = document.getElementById("viewed-image");
+    let oldsrc = view as HTMLImageElement;
+    oldsrc.src = src;
+    
+    if(type == "landscape"){
+      console.log("A landscape pic has been clicked")
+      view.style.height = "45vh";
+      shower.style.paddingTop = "20vmin";
+    }
+    else{
+      console.log("A landscape pic was not clicked")
+      view.style.height = "80vh";
+      shower.style.paddingTop = null;
+    }
+  
+  
+  }
 
+  close(event: any){
+    let shower = document.getElementById("shower");
+    shower.style.display = "none"
+   console.log("x has been clicked");
+  }
 }
