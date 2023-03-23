9990094ef54366a22db3b7883486243359076815
Martina <mckurin@gmail.com>
2023-03-23 23:05:33 +0000

    enable adding multiple files

--- np-lutherie/src/app/testimonial/testimonial.component.ts
+++ np-lutherie/src/app/testimonial/testimonial.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-testimonial',
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./testimonial.component.scss']
 })
 export class TestimonialComponent {
+
+  num = 0;
+
+  addImage(){
+    this.num = this.num + 1;
+    let newNum = this.num;
+    let x = document.createElement("INPUT");
+    x.setAttribute("type", "file");
+    x.setAttribute("id", "upload" + newNum);
+    x.setAttribute("name", "attachment" + newNum);
+    x.setAttribute("class", "image-field");
+    x.setAttribute("accept", "image/png, image/jpeg");
+    let div = document.getElementById("added-image");
+    div.appendChild(x); 
+    let y = document.createElement("button");
+    y.setAttribute("type", "button")
+    y.setAttribute("value", "upload" + newNum)
+    y.innerHTML = "Remove";
+    let connect = y.value;
+    y.onclick = function (){
+      console.log("remove button was clicked");
+      let element = document.getElementById(connect);
+      element.remove()
+      y.remove();
+    }
+    div.appendChild(y);
+  }
+
+  
+
+
 
 }
